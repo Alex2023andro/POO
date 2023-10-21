ab50a7f3c83c6ea4fe9a9b0f0ebf2b1b77645149
Alex2023andro <rsja1305@gmail.com>
2023-10-21 16:06:19 -0600

    Ejercicios 3 de Octubre

---
+++ proyectpoo/src/com/poo/EstudianteServiceImpl.java
@@ -0,0 +1,13 @@
+package com.poo;
+
+import java.util.List;
+
+import com.poo.model.Materia;
+
+public class EstudianteServiceImpl {
+
+    public double promedioFinal(List<Materia> materias) {
+        return 0;
+    }
+
+}
---
+++ proyectpoo/src/com/poo/IEstudianteService.java
@@ -0,0 +1,13 @@
+package com.poo;
+
+import java.util.List;
+
+import com.poo.model.Materia;
+
+public class IEstudianteService {
+
+    public double promedioFinal(List<Materia> materias) {
+        return 0;
+    }
+
+}
